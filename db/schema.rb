c073c31dbbe0ee9e8f1d96039a0003e45f58280e
pnn1997 <pnn1997@gmail.com>
2017-10-09 22:47:42 +0000

    Added comment

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20171006230906) do
-
+  # Defines bikes schema
   create_table "bikes", force: :cascade do |t|
     t.string   "bikeid"
     t.integer  "serialnumber"
