d1739bcf427f59541d3af1df16fe8ba1c97fcd06
tsteffens <tsteffen@tulane.edu>
2017-10-09 02:45:38 +0000

    Made database for bikes

--- db/migrate/20171006230906_create_bikes.rb
+++ db/migrate/20171006230906_create_bikes.rb
@@ -1,3 +1,4 @@
+=begin
 class CreateBikes < ActiveRecord::Migration
   def change
     create_table :bikes do |t|
@@ -8,6 +9,27 @@ class CreateBikes < ActiveRecord::Migration
       # Add fields that let Rails automatically keep track
       # of when movies are added or modified:
       t.timestamps
+    end
+  end
+end
+=end
+
+class CreateBikes < ActiveRecord::Migration
+  def change
+    create_table :bikes do |t|
+      t.string :bikeid
+      t.integer :serialnumber
+      t.string :rating
+      t.string :condition
+      t.datetime :maintenance
+      t.string :style
+      t.string :size
+      t.string :color
+      t.boolean :availability
+      t.datetime :lastcheck
+      t.string :location
+      t.float :fare
+      t.string :accessories
     end
   end
 end
