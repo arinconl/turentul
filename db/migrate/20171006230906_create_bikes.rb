85cdeb0dfa515a470175896b82c21e860b5fc84b
arinconl <arinconl@tulane.edu>
2017-10-18 05:35:41 +0000

    Styling changes across the board. Not final, but add some temporary flair. Table cleaned up to look nicer with formatting (auto capitalization if necessary). Bike pages now include a back link. Availability now shows location only if available (checked-out otherwise). Fare now correctly displays two decimal places.

--- db/migrate/20171006230906_create_bikes.rb
+++ db/migrate/20171006230906_create_bikes.rb
@@ -1,19 +1,3 @@
-=begin
-class CreateBikes < ActiveRecord::Migration
-  def change
-    create_table :bikes do |t|
-      t.string 'title'
-      t.string 'rating'
-      t.text 'description'
-      t.datetime 'release_date'
-      # Add fields that let Rails automatically keep track
-      # of when movies are added or modified:
-      t.timestamps
-    end
-  end
-end
-=end
-
 class CreateBikes < ActiveRecord::Migration
   def change
     create_table :bikes do |t|
