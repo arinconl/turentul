bf3ef447e4d9c4a053ff3c2ce4b871d9a245da04
pnn1997 <pnn1997@gmail.com>
2017-10-25 18:43:27 +0000

    Defined functions in renters controller and added demo views for index and show

--- app/controllers/renters_controller.rb
+++ app/controllers/renters_controller.rb
@@ -1,2 +1,8 @@
 class RentersController < ApplicationController
+    def index #Shows a list of the renters currently in the database (10 at a time)
+        @renters = Renter.page(params[:page]).per(10)
+    end
+    def show #Shows info about the renter
+        @renter = Renter.find(params[:id])
+    end
 end
