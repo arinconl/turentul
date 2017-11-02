35a65b23b1052bb63790350a8839b9aa22b6d8f5
arinconl <arinconl@tulane.edu>
2017-11-02 05:19:44 +0000

    Formatted add/edit forms. Generally formatted flashes. Formatted miniNavs.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171025181019) do
+ActiveRecord::Schema.define(version: 20171101004858) do
 
   create_table "bikes", force: :cascade do |t|
     t.string   "bikeid"
