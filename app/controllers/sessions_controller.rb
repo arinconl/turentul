5ed033b23319b1bf6abfc0bce7807fc36ebacdfc
pnn1997 <pnn1997@gmail.com>
2017-11-02 15:08:24 +0000

    Added a hack for creating session with facebook login. This may or may not work

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,14 @@
 class SessionsController < ApplicationController
   def create
     session[:logged_in] = true
-    renter = Renter.from_omniauth(env["omniauth.auth"])
+#    renter = Renter.from_omniauth(env["omniauth.auth"])
 =begin
     session[:renter_id] = renter.id
     session[:renter_name] = renter.renterName
 =end
     flash[:notice] = "Log in successful!"
-    redirect_to root_path
+    #redirect_to root_path
+    redirect_to "/auth/facebook", id: "sign_in"
   end
 
   def destroy
