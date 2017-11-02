c3ac51b7c54e031503a7dab944552f58764cf250
pnn1997 <pnn1997@gmail.com>
2017-11-02 14:23:47 +0000

    Added flash messages to session creation and destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,15 @@ class SessionsController < ApplicationController
     session[:logged_in] = true
     session[:renter_id] = renter.id
     session[:renter_name] = renter.renterName
+    flash[:notice] = "Log in successful!"
     redirect_to root_path
   end
 
   def destroy
     session[:renter_id] = nil
-    session[:logged_in] = false
+    session[:logged_in] = nil
     session[:renter_name] = nil
+    flash[:notice] = "Log out successful!"
     redirect_to root_path
   end
 end
