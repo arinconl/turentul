fcf540e2aec5b7d183a447c522ff2ec8d842b6ac
cmoroz1 <cmoroz1@tulane.edu>
2017-11-16 18:04:34 +0000

    Changed current_user method in application_controller so that Cucumber tests work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,13 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    @current_user ||= Renter.find(session[:renter_id]) if session[:renter_id]
+    # Checks to see if we are testing with Cucumber or not. If we are then it creates a 
+    # temp user with some fake data, so that the code in the views & controllers works properly
+    if session[:testing?] != true
+      @current_user ||= Renter.find(session[:renter_id]) if session[:renter_id]
+    else
+      @current_user = Renter.new(:renterID => 123456789012345678901, :renterName => "Chris Moroz", :email => "chrismoroz9@gmail.com", :phone => "(234)-394-1948", :cCN => "1234567890123456")
+    end
   end
   
   #Allows us to check if we are logged in
