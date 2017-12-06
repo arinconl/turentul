04f306dc8292435156e2a341e5c65a6121f5d759
arinconl <arinconl@tulane.edu>
2017-12-06 23:12:36 +0000

    Updated application layout, populated about page, added login dropdown, made preliminary navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,32 @@
 module ApplicationHelper
+	
+	#Note: need to convert to javascript
+	@@greetings = [
+		"hi", 
+		"hello", 
+		"howdy", 
+		"hey", 
+		"hiya", 
+		"'ello", 
+		"ahoy", 
+		"'sup", 
+		"yo", 
+		"g'day", 
+		"ciao", 
+		"aloha", 
+		"hola", 
+		"bonjour", 
+		"salud", 
+		"hallo", 
+		226.chr("UTF-8") + "llo",
+		"all" + 244.chr("UTF-8"),
+		];
+		
+	def greeting
+		@@greetings[rand(@@greetings.size)]
+	end
+	
+	def isAdmin
+		false
+	end
 end
