257d0ea4eefa84cee7034391ceb8b26a82cc6e3c
arinconl <arinconl@tulane.edu>
2017-12-07 01:25:34 +0000

    Moved greeting to javascript

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,31 +1,5 @@
 module ApplicationHelper
 	
-	#Note: need to convert to javascript
-	@@greetings = [
-		"hi", 
-		"hello", 
-		"howdy", 
-		"hey", 
-		"hiya", 
-		"'ello", 
-		"ahoy", 
-		"'sup", 
-		"yo", 
-		"g'day", 
-		"ciao", 
-		"aloha", 
-		"hola", 
-		"bonjour", 
-		"salud", 
-		"hallo", 
-		226.chr("UTF-8") + "llo",
-		"all" + 244.chr("UTF-8"),
-		];
-		
-	def greeting
-		@@greetings[rand(@@greetings.size)]
-	end
-	
 	def isAdmin
 		false
 	end
